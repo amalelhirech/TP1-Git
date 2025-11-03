ffc02508001f3b128f4564e37684da8ad2e75015
EL HIRECH Amal <elhirechamal@icloud.com>
2025-11-03 15:04:28 +0100

    exec5

---
+++ exec5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char s[5];
+    s[0] = 'H';
+    s[1] = 'e';
+    s[2] = 'y';
+    s[3] = '!';
+    s[4] = '\0';
+    printf("%s\n", s); 
+    return 0;
+}
+/* Il manque le caractere de fin*/
