ffb8acbdcc3e8dc37dc2d16787c984e24aa1e091
EL HIRECH Amal <elhirechamal@icloud.com>
2025-11-03 14:08:07 +0100

    compil4

---
+++ compil4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    printf("Test\n");
+    return 0;
+}
+/* .\compil4.c:3:10: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+ int main {
+          ^*/
+/* Il n' y a pas les parenthèse après le main() */
