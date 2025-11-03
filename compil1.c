ea540f05f4e0e2459174fa20722e5a11c6b2d552
EL HIRECH Amal <elhirechamal@icloud.com>
2025-11-03 13:56:07 +0100

    compil1

---
+++ compil1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello world\n");
+    return 0;
+}
+/* .\compil1.c: In function 'main':                                                                                                              
+.\compil1.c:5:5: error: expected ';' before 'return'
+     return 0;*/
+/*Donc la on vois qu'il manque le point virgule a la ligne 4*/
