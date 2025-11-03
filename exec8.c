1b765b8ddc9cfe39a499388d2428e19a823313f5
EL HIRECH Amal <elhirechamal@icloud.com>
2025-11-03 15:15:06 +0100

    exec8

---
+++ exec8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int t[3] = {10, 20, 30};
+    int i = 0;
+    printf("%d\n", t[i]); 
+    return 0;
+}
+/* pour le i on a un nombre negatif donc on sort du tableau */
