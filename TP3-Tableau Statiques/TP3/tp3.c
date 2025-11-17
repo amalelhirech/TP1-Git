43eb1caa6feaa9bc3a00b571ce0dc9d064093961
EL HIRECH Amal <elhirechamal@icloud.com>
2025-11-17 13:13:33 +0100

    tp3

--- TP3-Tableau Statiques/TP3/tp3.c
+++ TP3-Tableau Statiques/TP3/tp3.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+int main(){
+    int n;
+    printf("Entrez une taille entre 5 et 10");
+    scanf("%d", &n);
+    int width = n * n;
+    int max_height = 3 * n-1;
+    char img[20][20];
+
+
+    return 0;
+}
