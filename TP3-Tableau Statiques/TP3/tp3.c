2c2ca3b892e6059dd051d1961e5524df27137fa8
EL HIRECH Amal <elhirechamal@icloud.com>
2025-11-17 14:59:19 +0100

    tp3

--- TP3-Tableau Statiques/TP3/tp3.c
+++ TP3-Tableau Statiques/TP3/tp3.c
@@ -1,58 +1,71 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int n;
     printf("Entrez une taille entre 5 et 10: ");
     scanf("%d", &n);
 
-    if (n<5 || n>10){
+    if (n < 5 || n > 10)
+    {
         printf("Erreur la taille n'est pas bonne ");
     }
 
     int width = n * n;
-    int max_height = 3 * n-1;
+    int max_height = 3 * n - 1;
     char img[100][100];
 
-    for(int i = 0; i<max_height; i++){
-        for (int j = 0; j<width; j++){
+    for (int i = 0; i < max_height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             img[i][j] = ' ';
-            
         }
     }
 
     int mid = width / 2;
 
-    for (int i = 0; i<n; i++){
-        for (int j = mid-i; j<= mid + i; j++){
-            if ( j == mid-i || j == mid+i){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = mid - i; j <= mid + i; j++)
+        {
+            if (j == mid - i || j == mid + i)
+            {
                 img[i][j] = 'A';
             }
-            else {
+            else
+            {
                 img[i][j] = 'S';
             }
         }
     }
 
-     for (int j = 0; j<=mid-n; j++){
-        img[n-1][j] = '_';
+    for (int j = 0; j <= mid - n; j++)
+    {
+        img[n - 1][j] = '_';
     }
-    
-    for (int j = mid+n; j<width; j++){
-        img[n-1][j] = '_';
+
+    for (int j = mid + n; j < width; j++)
+    {
+        img[n - 1][j] = '_';
     }
-    
-   
 
+    for (int j = 0; j < width; j++)
+    {
+        if (j == 0 || j == width - 1)
+            img[n][j] = '"';
+        else
+            img[n][j] = 'S';
+    }
 
-     for (int i = 0; i<= max_height; i++){
-        for (int j = 0; j<=width; j++ ){
-            printf("%c", img[i][j] );
+    for (int i = 0; i <= max_height; i++)
+    {
+        for (int j = 0; j <= width; j++)
+        {
+            printf("%c", img[i][j]);
         }
         printf("\n");
     }
 
-   
-    
-    
     return 0;
 }
