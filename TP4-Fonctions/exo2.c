b8da10ec67b1f39befb0d75f04dcac5a49d7fa17
EL HIRECH Amal <elhirechamal@icloud.com>
2025-11-24 20:29:35 +0100

    exo2

--- TP4-Fonctions/exo2.c
+++ TP4-Fonctions/exo2.c
@@ -17,13 +17,13 @@ int saisirMontant()
 
 int montantValide(int montant)
 {
-    if (montant < 5 || montant > 1000 || montant % 5 != 0) {
+    if (montant < 5 || montant > 1000 || montant % 5 != 0)
+    {
         printf("Erreur : montant invalide\n");
         printf("Le montant doit etre entre 5 euros et 1000 euros, divisible par 5.\n");
-        return 0; 
+        return 0;
     }
-    return 1; 
-
+    return 1;
 }
 
 void calculDistribution(int montant)
@@ -41,37 +41,43 @@ void calculDistribution(int montant)
     montant = montant % 5;
 
     printf("Billets distribues :\n");
-    if (billets50 > 0) printf("%d billets de 50 euros\n", billets50);
-    if (billets20 > 0) printf("%d billets de 20 euros\n", billets20);
-    if (billets10 > 0) printf("%d billets de 10 euros\n", billets10);
-    if (billets5 > 0) printf("%d billets de 5 euros\n", billets5);
-
-
+    if (billets50 > 0)
+        printf("%d billets de 50 euros\n", billets50);
+    if (billets20 > 0)
+        printf("%d billets de 20 euros\n", billets20);
+    if (billets10 > 0)
+        printf("%d billets de 10 euros\n", billets10);
+    if (billets5 > 0)
+        printf("%d billets de 5 euros\n", billets5);
 }
 
 int main()
 {
-    afficherMenu();
     int choix = 0;
-    printf("Entrez un choix : \n");
-    scanf("%d", &choix);
-    
-    if (choix == 1) {
-        int montant = saisirMontant(); 
-        if(montantValide(montant))
-        {
-            printf("Montant a retirer : %d\n", montant);
-            calculDistribution(montant);
-        }
-        
-    }
-    else if (choix == 2) {
-        printf("Au revoir !\n");
-    }
-    else {
-        printf("Choix invalide.\n");
-    }
+    while (choix != 2)
+    {
+        afficherMenu();
+        printf("Entrez un choix : \n");
+        scanf("%d", &choix);
 
+        if (choix == 1)
+        {
+            int montant = saisirMontant();
+            if (montantValide(montant))
+            {
+                printf("Montant a retirer : %d\n", montant);
+                calculDistribution(montant);
+            }
+        }
+        else if (choix == 2)
+        {
+            printf("Au revoir !\n");
+        }
+        else
+        {
+            printf("Choix invalide.\n");
+        }
+    }
 
     return 0;
 }
