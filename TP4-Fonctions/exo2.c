a87b36e51ed795558070c00fbad3d864fa581ae9
EL HIRECH Amal <elhirechamal@icloud.com>
2025-11-24 17:31:23 +0100

    exo2

--- TP4-Fonctions/exo2.c
+++ TP4-Fonctions/exo2.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 
+void afficherMenu()
+{
+    printf("=== BANQUE - DISTRIBUTEUR ===\n");
+    printf("1 - Faire un retrait\n");
+    printf("2 - Quitter\n");
+}
+
+int main()
+{
+    afficherMenu();
+
+    return 0;
+}
