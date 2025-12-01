46a9150d351f365527632abfc3bb434dd074aa3a
EL HIRECH Amal <elhirechamal@icloud.com>
2025-12-01 15:01:05 +0100

    main_exo1

--- TP5/main_exo1.c
+++ TP5/main_exo1.c
@@ -18,6 +18,15 @@ int main()
         else if (choix == 2) {
             afficheResume(conso);
         }
+        else if (choix == 3){
+            if (sauvegarder(conso)) {
+                printf("Sauvegarde reussie dans consommation.txt\n");
+            } else {
+                printf("Erreur lors de la sauvegarde.\n");
+            }
+            quitter = 1;
+
+        }
         else 
         {
             printf("Choix invalide.\n");
