b1d43bf699bde4cdc2543a5872d25142e4d4a012
EL HIRECH Amal <elhirechamal@icloud.com>
2026-03-23 13:35:02 +0100

    question 1- erreur correction == 0

--- TP4_TU/TP4_TU.c
+++ TP4_TU/TP4_TU.c
@@ -1,7 +1,7 @@
 #include "TP4_TU.h"
 
 int estPair(int n) {
-    return n % 2 == 1;
+    return n % 2 == 0;
 }
 
 int max2(int a, int b) {
---
+++ TP4_TU_Test/TP4_TU_Test.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "../TP4_TU/TP4_TU.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TP4TUTest
+{
+	TEST_CLASS(TP4TUTest)
+	{
+	public:
+		
+		TEST_METHOD(TestEstPair)
+		{
+			Assert::AreEqual(estPair(4), 1)	;
+		}
+	};
+}
